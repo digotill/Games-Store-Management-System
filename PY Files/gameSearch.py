ca53c805b57fa779d83b265eca26cf724fd1107b
Digotill <digotill@gmail.com>
2025-12-03 11:04:21 +0000

    gameSearch function

--- PY Files/gameSearch.py
+++ PY Files/gameSearch.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 cur_path = os.path.dirname(__file__)
 
 def loadGameSearch(gameType):
@@ -11,13 +11,14 @@ def loadGameSearch(gameType):
                               next(reader)
                               for row in reader:
                                         game_id, title, platform, genre, purchase_date = row
-                                        Games[game_id] = {'title': title, 'platform': platform, 'genre': genre, 'purchase_date': purchase_date}
+                                        Games[game_id] = {'type': 'digital', 'title': title, 'platform': platform, 'genre': genre, 'purchase_date': purchase_date}
           if "board" in gameType:
                     with open(os.path.join(os.path.abspath(os.path.join(cur_path, os.pardir)), 'TXT Files', 'boardGameInfo.txt'), 'r', encoding='utf-8') as file:
                               reader = csv.reader(file)
                               next(reader)
                               for row in reader:
                                         game_id, title, players, genre, purchase_date = row
-                                        Games[game_id] = {'title': title, 'players': players, 'genre': genre, 'purchase_date': purchase_date}
+                                        Games[game_id] = {'type': 'board', 'title': title, 'players': players, 'genre': genre, 'purchase_date': purchase_date}
 
           return Games
+
