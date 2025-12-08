b8dafc966327bfa061927ca3cfec05ce11619095
Digotill <digotill@gmail.com>
2025-12-08 13:30:38 +0000

    New writing function

--- PY Files/database.py
+++ PY Files/database.py
@@ -1,6 +1,10 @@
 import os
 import csv
+
+from torch._export.db.examples import dictionary
+
 cur_path = os.path.dirname(__file__)
+
 
 def loadGameSearch(gameType):
           Games = {}
@@ -11,16 +15,17 @@ def loadGameSearch(gameType):
                               next(reader)
                               for row in reader:
                                         game_id, title, platform, genre, purchase_date = row
-                                        Games[game_id] = {'Type': 'digital', 'Title': title, 'Platform': platform, 'Genre': genre, 'Purchase Date': purchase_date}
+                                        Games[game_id] = {'Title': title, 'Platform': platform, 'Genre': genre, 'Purchase Date': purchase_date}
           if "board" in gameType:
                     with open(os.path.join(os.path.abspath(os.path.join(cur_path, os.pardir)), 'TXT Files', 'boardGameInfo.txt'), 'r', encoding='utf-8') as file:
                               reader = csv.reader(file)
                               next(reader)
                               for row in reader:
                                         game_id, title, players, genre, purchase_date = row
-                                        Games[game_id] = {'Type': 'board', 'Title': title, 'Players': players, 'Genre': genre, 'Purchase Date': purchase_date}
+                                        Games[game_id] = {'Title': title, 'Players': players, 'Genre': genre, 'Purchase Date': purchase_date}
 
           return Games
+
 
 def loadRental():
           Rentals = {}
@@ -28,7 +33,7 @@ def loadRental():
                     reader = csv.reader(file)
                     next(reader)
                     for row in reader:
-                              game_id, start, end, user_id  = row
+                              game_id, start, end, user_id = row
                               Rentals[game_id] = {'Start': start, 'End': end, 'User ID': user_id}
           return Rentals
 
@@ -40,3 +45,12 @@ def loadGame(gameName):
                               return game_id, game_data
           return None
 
+
+def write_dict_to(new_dict, filename):
+          with open(os.path.join(os.path.abspath(os.path.join(cur_path, os.pardir)), 'TXT Files', filename), 'w', newline='', encoding="UTF-16") as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([])
+                    for primary_key, inner_dict in new_dict.items():
+                              row = [primary_key]
+                              row.extend(inner_dict.values())
+                              writer.writerow(row)
